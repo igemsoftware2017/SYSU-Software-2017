0e0cdec5dfecb4fce708c3b5102f370d2623ec81
freedom-zjw <709075442@qq.com>
2017-09-05 21:39:06 +0800

    update example.py

--- server/example.py
+++ server/example.py
@@ -11,26 +11,3 @@ dao.table_init()
 dao.import_all_input_files(current_path + os.sep + "database" + os.sep + "preload")
 
 dao.close()
-"""
-
-
-# 以下语句只需运行一次~ 将数据导入到数据库
-dao.import_all_input_files('E:\\data\\')
-
-dataHelper.prepare_input_files('D:\\data\\')
-
-# 这里调用EpredCaller运行epred
-# 另外Model和Rfile文件夹需要自行复制
-os.system(
-    r'java -jar xxx.jar -i D:\data\ -o D:\output\ -p-area all -p-duration annual -p-year 2016 -rr')
-
-dataHelper.collect_output_files('D:\\output\\')
-
-# 将数据库保存的输出文件导出到 D:\output_clone
-dataHelper.export_output_files('D:\\output_clone\\')
-
-dao.close()
-
-
-
-"""
