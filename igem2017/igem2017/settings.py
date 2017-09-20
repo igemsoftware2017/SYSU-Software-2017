5625c19000183cab2b045eb9a0dc34d35c367074
freedom-zjw <709075442@qq.com>
2017-09-21 00:26:35 +0800

    update load parts(untest)

--- igem2017/igem2017/settings.py
+++ igem2017/igem2017/settings.py
@@ -74,6 +74,8 @@ WSGI_APPLICATION = 'igem2017.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
--- igem2017/sdin/tools/pre_load_data.py
+++ igem2017/sdin/tools/pre_load_data.py
@@ -31,19 +31,22 @@ def load_parts(parts_floder_path):
                 if (row_cnt == 1):
                     continue
                 else:
-                    quary = Parts.objects.filter(Name =row[0])
-                    if not quary.exists():
-                        try:
-                            Parts.objects.create(
-                                Name = row[0],
-                                Description = row[1],
-                                Type = row[2],
-                                Subpart = row[3],
-                                Safety = row[4],
-                                Sequence = row[5]
-                            )
-                        except:
-                            pass
+                    try:
+                        new_part = Parts(
+                            Name = row[0],
+                            Description = row[1],
+                            Type = row[2],
+                            Safety = row[4],
+                            Sequence  = row[5]
+                        )
+                        new_part.save();
+                        if row[3] != "":
+                            s = row[3]
+                            s = re.split('[, ]', row[3][1:-1])
+                            for part_name in s:
+                                new_part.Subparts.add(part_name)
+                    except:
+                        pass
 
 #load works data
 def load_works(works_floder_path):
@@ -57,27 +60,25 @@ def load_works(works_floder_path):
             for row in csv_reader:
                 row_cnt += 1
                 if (row_cnt > 1):
-                    quary = Works.objects.filter(TeamID=row[0])
-                    if not quary.exists():
-                        try:
-                            Works.objects.create(
-                                TeamID = int(row[0]),
-                                Teamname = row[1],
-                                Region = row[2],
-                                Country = row[3],
-                                Track = row[4],
-                                Section = row[5],
-                                Size = int(row[6]),
-                                Status = row[7],
-                                Year = int(row[8]),
-                                Wiki = row[9],
-                                Medal = row[10],
-                                Award = row[11],
-                                Name = row[12],
-                                Use_parts = row[13],
-                            )
-                        except:
-                            pass
+                    try:
+                        Works.objects.create(
+                            TeamID = int(row[0]),
+                            Teamname = row[1],
+                            Region = row[2],
+                            Country = row[3],
+                            Track = row[4],
+                            Section = row[5],
+                            Size = int(row[6]),
+                            Status = row[7],
+                            Year = int(row[8]),
+                            Wiki = row[9],
+                            Medal = row[10],
+                            Award = row[11],
+                            Name = row[12],
+                            Use_parts = row[13],
+                        )
+                    except:
+                        pass
 
 
 def pre_load_data(currentpath):
