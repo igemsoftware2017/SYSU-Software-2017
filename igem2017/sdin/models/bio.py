59a692822e2e002c59a28f42081f425420e74d27
StrickerLee <lixiangabc@hotmail.com>
2017-10-08 22:17:44 +0800

    change api

--- igem2017/sdin/models/bio.py
+++ igem2017/sdin/models/bio.py
@@ -7,12 +7,18 @@ class Parts(models.Model):
     Name = models.CharField(max_length = 50, unique = True, db_index = True)
     Description = models.CharField(max_length = 100)
     Type = models.CharField(max_length = 20)
-    Subparts = models.ManyToManyField('Parts')
     Safety = models.CharField(max_length = 500)
     Sequence = models.TextField()
 
     def __str__(self):
         return "%s : %s" % (self.Name, self.Description)
+
+class SubParts(models.Model):
+    parent = models.ForeignKey('Parts', on_delete = models.CASCADE)
+    child = models.ForeignKey('Parts', on_delete = models.CASCADE)
+
+    def __str__(self):
+        return "%s contains %s" % (self.parent.Name, self.child.Name)
 
 class Circuit(models.Model):
     Name = models.CharField(max_length = 50, unique = True, db_index = True)
