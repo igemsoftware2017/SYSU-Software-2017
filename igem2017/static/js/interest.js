f89ada3992b866b95666ea249d54f4ebc4e132e5
SgLy <775150558@qq.com>
2017-10-28 14:21:48 +0800

    init interest page

--- igem2017/static/js/interest.js
+++ igem2017/static/js/interest.js
@@ -1,25 +1,54 @@
+function insertInterest(x, y) {
+    let a = $('<div class="interest"></div>').appendTo('.interests');
+    a.css({
+        top: `calc(${y}px + 0.5rem)`,
+        left: `calc(${x}px + 0.5rem)`,
+        position: 'absolute'
+    });
+}
+let positions = [];
+let sqr = (x) => x * x;
+let distance = (a, b) => Math.sqrt(sqr(a.x - b.x) + sqr(a.y - b.y));
+let valid = (p, t) => positions.reduce((r, v) => r && distance(p, v) > t, true);
 let chosenInterest = new Set();
 let options = [
-    { text: 'water', top: 250, left: 300 },
-    { text: 'cancer', top: 300, left: 450 },
-    { text: 'cell', top: 250, left: 600 },
-    { text: 'superman', top: 300, left: 750 }
+    { text: 'Environment', option: 'environment'},
+    { text: 'Disease', option: 'disease'},
+    { text: 'Food', option: 'food'},
+    { text: 'Energy', option: 'energy'},
+    { text: 'Hardware', option: 'hardware'},
+    { text: 'Software', option: 'software'},
+    { text: 'Industry', option: 'industry'},
+    { text: 'Pollution', option: 'pollution'},
+    { text: 'Agriculture', option: 'agriculture'},
+    { text: 'Art', option: 'art'},
+    { text: 'Heavy metal', option: 'heavy metal'}
 ];
-options.forEach(function (o, i) {
-    let id = 'interest-' + i;
-    $('.interests').append('<div class="interest" id="' + id + '">' + o.text + '</div>');
+let width = $('.interests').width();
+let height = $('.interests').height();
+let em = ($('.container').offset().top - $('.container').position().top) / 6;
+console.log(width, height);
+options.forEach((o, i) => {
+    let id = 'interest-' + i, text = o.text;
+    $('.interests').append(`<div class="interest" id="${id}">${text}</div>`);
     o.item = $('#' + id);
     let item = o.item;
+    let p = { x: 0, y: 0 };
+    do {
+        p.x = Math.ceil(Math.random() * width);
+        p.y = Math.ceil(Math.random() * height);
+    } while (valid(p, 6.5 * em) === false);
+    positions.push(p);
     item
         .css({
-            top: o.top,
-            left: o.left
+            top: p.y,
+            left: p.x
         })
         .data('origin', { top: o.top, left: o.left })
         .data('id', id)
         .data('chosen', false)
         .data('hover', false)
-        .data('option', o.text)
+        .data('option', o.option)
         .data('timerId', setInterval(function() {
             if (!item.data('chosen') && !item.data('hover')) {
                 let newTop = 0, newLeft = 0;
@@ -33,36 +62,28 @@ options.forEach(function (o, i) {
                     left: newLeft
                 });
             }
-        }, 1000))
+        }, 1100))
         .on('mouseenter', function() {
             if (!item.data('chosen'))
                 item
-                    .css({
-                        transform: 'scale(1.2, 1.2)'
-                    })
+                    .addClass('interest-hover')
                     .data('hover', true)
                     .stop();
         })
         .on('mouseleave', function() {
             if (!item.data('chosen'))
                 item
-                    .css({
-                        transform: 'scale(1, 1)'
-                    })
+                    .removeClass('interest-hover')
                     .data('hover', false);
         })
         .on('click', function() {
             if (!item.data('chosen')) {
                 item.data('chosen', true);
-                item.css({
-                    boxShadow: '0 0 2em 0 rgba(0, 0, 0, 0.7)'
-                });
+                item.addClass('interest-chosen');
                 chosenInterest.add(item.data('option'));
             } else {
                 item.data('chosen', false);
-                item.css({
-                    boxShadow: '0 0 1em 0 rgba(0, 0, 0, 0.7)'
-                });
+                item.removeClass('interest-chosen');
                 chosenInterest.delete(item.data('option'));
             }
         });
@@ -75,11 +96,23 @@ $('#reset')
                 o.item
                     .data('chosen', false)
                     .data('hover', false)
-                    .css({
-                        boxShadow: '0 0 1em 0 rgba(0, 0, 0, 0.7)',
-                        transform: 'scale(1.0, 1.0)'
-                    });
+                    .removeClass('interest-chosen interest-hover');
                 chosenInterest.delete(o.item.data('option'));
             }
+        });
+    });
+
+$('#finish')
+    .on('click', () => {
+        let data = [];
+        chosenInterest.forEach((v) => { data.push(v); });
+        let postData = {
+            csrfmiddlewaretoken: $('[name=csrfmiddlewaretoken]').val(),
+            data: JSON.stringify({ interest: data })
+        };
+        console.log(postData);
+        $.post('/api/interest', postData, (r) => {
+            if (r.success === true)
+                window.location.href = '/';
         });
     });
