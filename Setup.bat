55d3cd375f8c86736696f29c45156ee87613937b
freedom-zjw <709075442@qq.com>
2017-10-31 19:39:06 +0800

    add

---
+++ Setup.bat
@@ -0,0 +1,11 @@
+﻿python3 -m pip install --upgrade pip
+cd tools
+python3 tools/ReqSetup.py
+python3 tools/DbInit.py
+cd ../
+cd igem2017
+python3 manage.py makemigrations sdin
+python3 manage.py migrate 
+python3 manage.py shell < init.py
+cd ../
+
---
+++ runserver.bat
@@ -0,0 +1,2 @@
+cd igem2017
+python3 manage.py runserver
