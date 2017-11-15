3844f71cef035a47fa598aed0dd2b94732ef0362
SgLy <775150558@qq.com>
2017-11-15 13:55:31 +0000

    add log

--- igem2017/igem2017/settings.py
+++ igem2017/igem2017/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sj@x-_lm)95xc_%b+d%nmer95a=e0j$vgjc4v3m#js$aj^rz(9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', 'sdin.sysusoftware.info']
 
@@ -138,3 +138,93 @@ AUTH_USER_MODEL = 'sdin.User'
 
 # Login required redirect
 LOGIN_URL = '/login/'
+
+# Log
+
+
+import logging
+import django.utils.log
+import logging.handlers
+ 
+ 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+       'standard': {
+            'format': '%(asctime)s [%(threadName)s:%(thread)d] [%(name)s:%(lineno)d] [%(module)s:%(funcName)s] [%(levelname)s]- %(message)s'}  #日志格式 
+    },
+    'filters': {
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+        },
+        'default': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': '/home/smartgirl/IGEM2017-SYSU.Software/igem2017/log/all.log',     #日志输出文件
+            'maxBytes': 1024*1024*5,                  #文件大小 
+            'backupCount': 5,                         #备份份数
+            'formatter':'standard',                   #使用哪种formatters日志格式
+        },
+        'error': {
+            'level':'ERROR',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': '/home/smartgirl/IGEM2017-SYSU.Software/igem2017/log/error.log',
+            'maxBytes':1024*1024*5,
+            'backupCount': 5,
+            'formatter':'standard',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        'request_handler': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': '/home/smartgirl/IGEM2017-SYSU.Software/igem2017/log/script.log', 
+            'maxBytes': 1024*1024*5, 
+            'backupCount': 5,
+            'formatter':'standard',
+        },
+        'scprits_handler': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename':'/home/smartgirl/IGEM2017-SYSU.Software/igem2017/log/script.log', 
+            'maxBytes': 1024*1024*5, 
+            'backupCount': 5,
+            'formatter':'standard',
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['default', 'console'],
+            'level': 'DEBUG',
+            'propagate': False 
+        },
+        'django.request': {
+            'handlers': ['request_handler'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'scripts': { 
+            'handlers': ['scprits_handler'],
+            'level': 'INFO',
+            'propagate': False
+        },
+        'sdin.views': {
+            'handlers': ['default', 'error'],
+            'level': 'DEBUG',
+            'propagate': True
+        },
+        'sdin.util':{
+            'handlers': ['error'],
+            'level': 'ERROR',
+            'propagate': True
+        }
+    } 
+}
